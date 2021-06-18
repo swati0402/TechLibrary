c973dbe57ff9d112463b84f790ce7554a047cde0
Swati Ojha <Swati_Ojha@aisreview.com>
2021-06-18 16:37:27 -0400

    updated testcase

--- TechLibraryExercise-master/TechLibrary.Test/TechLibrary.Test/Controllers/BooksControllerTests.cs
+++ TechLibraryExercise-master/TechLibrary.Test/TechLibrary.Test/Controllers/BooksControllerTests.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TechLibrary.ResourceParameters;
 using TechLibrary.Services;
 
 namespace TechLibrary.Controllers.Tests
@@ -18,6 +19,7 @@ namespace TechLibrary.Controllers.Tests
         private  Mock<IBookService> _mockBookService;
         private  Mock<IMapper> _mockMapper;
         private NullReferenceException _expectedException;
+        BooksResourceParams bookParams;
 
         [OneTimeSetUp]
         public void TestSetup()
@@ -32,14 +34,14 @@ namespace TechLibrary.Controllers.Tests
         public async Task GetAllTest()
         {
             //  Arrange
-            _mockBookService.Setup(b => b.GetBooksAsync()).Returns(Task.FromResult(It.IsAny<List<Domain.Book>>()));
+            _mockBookService.Setup(b => b.GetBooksAsync(bookParams)).Returns(Task.FromResult(It.IsAny<List<Domain.Book>>()));
             var sut = new BooksController(_mockLogger.Object, _mockBookService.Object, _mockMapper.Object);
 
             //  Act
-            var result = await sut.GetAll();
+            var result = await sut.GetAll(bookParams);
 
             //  Assert
-            _mockBookService.Verify(s => s.GetBooksAsync(), Times.Once, "Expected GetBooksAsync to have been called once");
+            _mockBookService.Verify(s => s.GetBooksAsync(bookParams), Times.Once, "Expected GetBooksAsync to have been called once");
         }
     }
 }
